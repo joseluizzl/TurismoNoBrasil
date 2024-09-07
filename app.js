361677865c582293b8dd0fa9f7a302e6b2f029fd
José Luiz Barbosa <162759465+joseluizzl@users.noreply.github.com>
2024-09-07 10:19:43 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+function pesquisar() {
+    // Essa função é responsável por realizar uma pesquisa e exibir os resultados na página.
+  
+    // Obtém a seção onde os resultados da pesquisa serão inseridos.
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+    console.log(campoPesquisa)
+  
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar algo!</p>"
+        return
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa.
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+  
+    // Itera sobre cada destino na lista de destinos.
+    for (let destino of destinos) {
+        titulo = destino.titulo.toLowerCase()
+        descricao = destino.descricao.toLowerCase()
+        if (titulo.includes(campoPesquisa) || 
+        descricao.includes(campoPesquisa)) {
+            // Cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+              <h2><a href="#" target="_blank">${destino.titulo}</a></h2>
+              <p class="descricao-meta">${destino.descricao}</p>
+              <a href="${destino.link}" target="_blank">Mais informações</a>
+            </div>
+          `;
+        }
+        console.log(destino.titulo.includes(campoPesquisa))
+     
+    }
+  
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+    // Insere o HTML gerado na seção de resultados.
+    section.innerHTML = resultados;
+  }
+
+
---
+++ dados.js
@@ -0,0 +1,52 @@
+let destinos = [
+    {
+        titulo: "Foz do Iguaçu, Paraná",
+        descricao: "Foz do Iguaçu é uma cidade brasileira localizada na tríplice fronteira entre Brasil, Argentina e Paraguai. Além das famosas Cataratas do Iguaçu, a cidade oferece outras atrações como o Parque das Aves e a Itaipu Binacional.",
+        link: "https://pt.wikipedia.org/wiki/Foz_do_Iguaçu"
+    },
+    {
+        titulo: "Fernando de Noronha, Pernambuco",
+        descricao: "Fernando de Noronha é um arquipélago brasileiro localizado no Oceano Atlântico, conhecido por suas praias paradisíacas, águas cristalinas e rica vida marinha. O arquipélago é um dos destinos turísticos mais exclusivos do Brasil, oferecendo diversas atividades como mergulho, snorkeling, trilhas e observação de tartarugas marinhas.",
+        link: "https://pt.wikipedia.org/wiki/Fernando_de_Noronha"
+    },
+    {
+        titulo: "Ouro Preto, Minas Gerais",
+        descricao: "Ouro Preto é uma cidade histórica localizada em Minas Gerais, conhecida por sua rica arquitetura colonial e por ter sido um dos principais centros de mineração durante o período do Brasil colonial. A cidade abriga diversas igrejas, museus e casarões históricos, que contam a história da região.",
+        link: "https://pt.wikipedia.org/wiki/Ouro_Preto"
+    },
+    {
+        titulo: "Lençóis Maranhenses, Maranhão",
+        descricao: "Os Lençóis Maranhenses são um parque nacional localizado no estado do Maranhão, famoso por suas lagoas cristalinas formadas entre dunas de areia branca. O parque é um verdadeiro paraíso natural, ideal para quem busca contato com a natureza e paisagens exuberantes.",
+        link: "https://pt.wikipedia.org/wiki/Len%C3%A7%B3is_Maranhenses"
+    },
+    {
+        titulo: "Bonito, Mato Grosso do Sul",
+        descricao: "Bonito é um município brasileiro localizado no estado de Mato Grosso do Sul, conhecido por suas águas cristalinas e diversas opções de ecoturismo. A cidade oferece diversas atividades como flutuação em rios, mergulho em cavernas, trilhas e observação de animais.",
+        link: "https://pt.wikipedia.org/wiki/Bonito_(Mato_Grosso_do_Sul)"
+    },
+    {
+        titulo: "Chapada Diamantina, Bahia",
+        descricao: "A Chapada Diamantina é uma região montanhosa no estado da Bahia, conhecida por suas paisagens exuberantes, cachoeiras, trilhas e cavernas. A cidade de Lençóis é o principal ponto de partida para explorar a região.",
+        link: "https://pt.wikipedia.org/wiki/Chapada_Diamantina"
+    },
+    {
+        titulo: "Pantanal, Mato Grosso e Mato Grosso do Sul",
+        descricao: "O Pantanal é o maior wetland contínuo do planeta e um dos biomas mais ricos em biodiversidade do mundo. A fauna e a flora são exuberantes, com a possibilidade de avistar diversas espécies de animais, como onças, jacarés e capivaras.",
+        link: "https://pt.wikipedia.org/wiki/Pantanal"
+    },
+    {
+        titulo: "Jericoacoara, Ceará",
+        descricao: "Jericoacoara é uma vila de pescadores que se tornou um dos destinos turísticos mais procurados do Brasil. Suas praias paradisíacas, dunas e lagoas atraem turistas do mundo todo.",
+        link: "https://pt.wikipedia.org/wiki/Jericoacoara"
+    },
+    {
+        titulo: "Paraty, Rio de Janeiro",
+        descricao: "Paraty é uma cidade histórica localizada no estado do Rio de Janeiro, conhecida por suas ruas de paralelepípedos, casarões coloniais e belas praias. A cidade é um Patrimônio Histórico Nacional e um dos destinos mais charmosos do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Paraty"
+    },
+    {
+        titulo: "Jalapão, Tocantins",
+        descricao: "O Jalapão é uma região no estado do Tocantins com paisagens exuberantes, como cânions, cachoeiras, dunas e fervedouros. É um destino ideal para quem busca aventura e contato com a natureza.",
+        link: "https://pt.wikipedia.org/wiki/Jalapão"
+    }
+];
